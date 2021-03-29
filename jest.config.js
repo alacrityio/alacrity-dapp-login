6e21d440467dd4b19ce98c01443096f8afdd91e0
joshglogau <josh.glogau@siliconswamp.io>
2021-03-29 16:42:29 -0400

    Remove __tests__

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,5 @@ module.exports = {
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest'
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(t|j)s$',
   moduleFileExtensions: ['ts', 'js', 'json', 'node']
 }
