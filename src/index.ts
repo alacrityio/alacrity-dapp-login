6e6eccb398ae66b5e8a1c529ea8a0f4e59c58f53
joshglogau <josh.glogau@siliconswamp.io>
2021-03-29 17:14:27 -0400

    crypto import

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-const CryptoJS = require("crypto-js");
+// import CryptoJS = require("crypto-js");
+// import CryptoJS from "crypto-js";
+// import * as CryptoJS from 'crypto-js';  
+// import AES from 'crypto-js/aes';
+var AES = require("crypto-js/aes");
 
 export default class DappLoginService {
     open(data : string, error? : boolean){
@@ -8,7 +12,7 @@ export default class DappLoginService {
 
         if (pin){
             let decrypted : any;
-            try   { decrypted = JSON.parse(CryptoJS.AES.decrypt(data, pin).toString(CryptoJS.enc.Utf8)); }
+            try   { decrypted = JSON.parse(AES.decrypt(data, pin).toString('utf8')); }
             catch { console.log("DECRYPT FAILED, TRY AGAIN 1"); if (pin) this.open(data, true); }
 
             if (decrypted && decrypted.email && decrypted.password && decrypted.mnemonic){
